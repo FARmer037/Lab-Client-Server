bfd9ef11f10633ffef95d2f5b809251779c90ae6
FARmer037 <mustofa0367@gmail.com>
2020-01-16 01:21:40 +0700

    delete function convert object to array

--- lab-1/person.js
+++ lab-1/person.js
@@ -17,21 +17,20 @@ var obj = people.reduce((obj, current) => ({...obj, [current.name]: current.age}
 
 console.log(obj)
 
-var arr = Object.keys(obj)
-    .map(function (key) {
-        return obj[key];
-    });
-
-console.log(arr)
-
 var youngerThan = (x) => {
     let young = people.filter(person => person.age < x);
-    console.log(young)
+
+    for(let i = 0; i < young.length; i++) {
+        console.log(young[i].name)
+    }
 }
 
 var olderThan = (x) => {
     let young = people.filter(person => person.age > x);
-    console.log(young)
+    
+    for(let i = 0; i < young.length; i++) {
+        console.log(young[i].name)
+    }
 }
 
 youngerThan(40)
