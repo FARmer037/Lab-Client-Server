bf3f2c6a7752374e47557f49063bea619a562c42
mustofa <mustofa_sachi@hotmail.com>
2020-02-13 03:52:18 +0700

    cookie

--- lab-5/index.js
+++ lab-5/index.js
@@ -1,13 +1,27 @@
 let express = require('express'),
-    app = express()
+    app = express(),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser')
 const PORT = 80
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.use(express.static('./public'))
+app.use(cookieParser('foobar'))
+
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.get('/getCk', (req, res) => {
+    res.send('' + req.cookies.name + '' + req.cookies.surname)
 })
 
-app.get('/foo', (req, res) => {
-    res.send('Foo')
+app.get('/setCk', (req, res) => {
+    res.cookie('name', 'John')
+    res.cookie('surname', 'Wick')
+    res.send('Set cookie done!')
+})
+
+app.post('/add', urlencodedParser, (req, res) => {
+    let result = parseInt(req.body.a) + parseInt(req.body.b)
+    res.send('Result: ' + result)
 })
 
 app.listen(PORT, () => {
